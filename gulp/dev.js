2dff45ce6df98854c611739bfe94757334604360
IkkeWa <xikkewa@gmail.com>
2016-08-28 14:42:17 +0200

    chore: add lint task to the dev task

--- gulp/dev.js
+++ gulp/dev.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 
 var config = require('../config_gulp');
 
-gulp.task('dev', ['serve', 'markup', 'watchify'], function() {
-  gulp.watch('lib/**/*.js', ['browserify']);
+gulp.task('dev', ['serve', 'markup', 'lint', 'watchify'], function() {
+  gulp.watch('lib/**/*.js', ['lint', 'browserify']);
   gulp.watch(config.markup.srcPaths, ['markup']);
 });
