1bf2186e8b0984a6e35ac72f9d7137dd53ed0c4c
IkkeWa <xikkewa@gmail.com>
2016-08-30 07:55:49 +0200

    chore: remove old original example js files and rename es6 versions

--- examples/menu.js
+++ examples/menu.js
@@ -1,76 +1,160 @@
-var game = new Phaser.Game(650, 350, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
-var slickUI;
-function preload() {
-    slickUI = game.plugins.add(Phaser.Plugin.SlickUI);
+'use strict';
+
+function main() {
+  var game = new Phaser.Game(650, 350, Phaser.CANVAS, 'gamecanvas', {
+    preload: preload,
+    create: create,
+    update: update
+  });
+
+  var SLICKUI = Phaser.Plugin.SlickUI;
+  var sui;
+
+  /**
+   * Preload
+   *
+   *  - load the Plugin
+   *  - load images
+   *  - load JSON for Slick-UI
+   */
+  function preload() {
+    sui = game.plugins.add(SLICKUI);
+
     game.load.image('menu-button', 'assets/ui/menu.png');
     game.load.image('backdrop', 'assets/backdrop.png');
-    slickUI.load('assets/ui/kenney/kenney.json');
-}
-function create() {
-    game.add.sprite(0,-125,'backdrop');
-    var button, panel, menuButton;
-    slickUI.add(panel = new SlickUI.Element.Panel(game.width - 156, 8, 150, game.height - 16));
-    panel.add(new SlickUI.Element.Text(10,0, "Menu")).centerHorizontally().text.alpha = 0.5;
-    panel.add(button = new SlickUI.Element.Button(0,game.height - 166, 140, 80)).events.onInputUp.add(function () {
-        console.log('Clicked save game');
-    });
 
-    button.add(new SlickUI.Element.Text(0,0, "Save game")).center();
+    sui.load('assets/ui/kenney/kenney.json');
+  }
 
-    panel.add(button = new SlickUI.Element.Button(0,game.height - 76, 140, 40));
-    button.add(new SlickUI.Element.Text(0,0, "Close")).center();
+  /**
+   * Create
+   *
+   */
+  function create() {
+    game.add.sprite(0, -125, 'backdrop');
 
+    // shorten some references
+    var easeMode = Phaser.Easing.Exponential.Out;
+    // vars later used
+    var basePosition;
+
+    // start building the menu
+    // order is important, as references (ex. to game) is needed
+    // during building elements, so define every element and
+    // than later stack them in the right order
+
+    // all texts
+    var menuText  = new SLICKUI.Text(game, 10, 0, 'Menu');
+    var saveText  = new SLICKUI.Text(game, 0, 0, 'Save Game');
+    var closeText = new SLICKUI.Text(game, 0, 0, 'Close');
+
+    // all buttons
+    var saveBtn = new SLICKUI.Button(game,
+        0, game.height - 166, 140, 80); // x, y, w, h
+    var closeBtn = new SLICKUI.Button(game,
+        0, game.height - 76, 140, 40); // x, y, w, h
+    var menuBtn = new SLICKUI.DisplayObject(game,
+        game.width - 45, 8, game.make.sprite(0, 0, 'menu-button'));
+        // x, y, dp-obj (w, h)
+
+    // all checkboxes
+    var check1 = new SLICKUI.Checkbox(game,
+        0, 100, SLICKUI.Checkbox.TYPE_CROSS);
+    var check2 = new SLICKUI.Checkbox(game,
+        50, 100, SLICKUI.Checkbox.TYPE_CROSS);
+    var check3 = new SLICKUI.Checkbox(game,
+        100, 100, SLICKUI.Checkbox.TYPE_CHECKBOX);
+
+    // the main PANEL
+    var panel = new SLICKUI.Panel(game,
+        game.width - 156, 8, 150, game.height - 16);
+
+    // add "root to leafes"
+    sui.add(panel);
+    sui.add(menuBtn);
+
+    panel.add(menuText);
+    panel.add(saveBtn);
+    panel.add(closeBtn);
+    panel.add(check1);
+    panel.add(check2);
+    panel.add(check3);
+
+    saveBtn.add(saveText);
+    closeBtn.add(closeText);
+
+    // after everything is addded, we can modify and "extend"
     panel.visible = false;
-    var basePosition = panel.x;
+    basePosition = panel.x;
 
-    slickUI.add(menuButton = new SlickUI.Element.DisplayObject(game.width - 45, 8, game.make.sprite(0, 0, 'menu-button')));
-    menuButton.inputEnabled = true;
-    menuButton.input.useHandCursor = true;
-    menuButton.events.onInputDown.add(function () {
-        if(panel.visible) {
-            return;
-        }
-        panel.visible = true;
-        panel.x = basePosition + 156;
-        game.add.tween(panel).to( {x: basePosition}, 500, Phaser.Easing.Exponential.Out, true).onComplete.add(function () {
-            menuButton.visible = false;
-        });
-        slickUI.container.displayGroup.bringToTop(panel.container.displayGroup);
-    }, this);
-
-    button.events.onInputUp.add(function () {
-        game.add.tween(panel).to( {x: basePosition + 156}, 500, Phaser.Easing.Exponential.Out, true).onComplete.add(function () {
-            panel.visible = false;
-            panel.x -= 156;
-        });
-        menuButton.visible = true;
+    saveBtn.events.onInputUp.add(function() {
+      console.log('Clicked Save Game');
     });
 
-    var cb1, cb2;
-    panel.add(cb1 = new SlickUI.Element.Checkbox(0,100, SlickUI.Element.Checkbox.TYPE_RADIO));
-    cb1.events.onInputDown.add(function () {
-        if(cb1.checked && cb2.checked) {
-            cb2.checked = false;
-        }
-        if(!cb1.checked && !cb2.checked) {
-            cb1.checked = true;
-        }
+    menuBtn.inputEnabled = true;
+    menuBtn.input.useHandCursor = true;
+    menuBtn.events.onInputDown.add(function() {
+      // on menu button click
+      if(panel.visible) {
+        return;
+      }
+
+      panel.visible = true;
+      panel.x = basePosition + 156;
+
+      game.add.tween(panel)
+          .to({ x: basePosition }, 500, easeMode, true)
+          .onComplete.add(function() {
+            menuBtn.visible = false;
+          });
+
+      sui.container.displayGroup
+                   .bringToTop(panel.container.displayGroup);
     }, this);
 
-    panel.add(cb2 = new SlickUI.Element.Checkbox(50,100, SlickUI.Element.Checkbox.TYPE_RADIO));
-    cb2.events.onInputDown.add(function () {
-        if(cb1.checked && cb2.checked) {
-            cb1.checked = false;
-        }
-        if(!cb1.checked && !cb2.checked) {
-            cb2.checked = true;
-        }
+    closeBtn.events.onInputDown.add(function() {
+      if(!panel.visible) {
+        return;
+      }
+
+      game.add.tween(panel)
+          .to({ x: basePosition + 156 }, 500, easeMode, true)
+          .onComplete.add(function() {
+            panel.x -= 156;
+            panel.visible = false;
+            menuBtn.visible = true;
+          });
     }, this);
 
-    panel.add(new SlickUI.Element.Checkbox(100,100));
+    saveText.center();
+    closeText.center();
+
+    function checkboxBehaviour(chk1, chk2) {
+      return function() {
+        if(chk1.checked && chk2.checked) {
+          chk2.checked = false;
+        }
+
+        if(!chk1.checked && !chk2.checked) {
+          chk1.checked = true;
+        }
+      }
+    }
+
+    check1.events.onInputDown.add(
+        checkboxBehaviour(check1, check2), this);
+
+    check2.events.onInputDown.add(
+        checkboxBehaviour(check2, check1), this);
+  }
+
+  /**
+   * Update
+   */
+  function update() {
+
+  }
 }
-function update() {
 
-}
-
-
+// run the init in own scope
+main();
--- examples/text-field.js
+++ examples/text-field.js
@@ -1,25 +1,80 @@
-var game = new Phaser.Game(650, 350, Phaser.CANVAS, '', { preload: preload, create: create });
-var slickUI;
-function preload() {
-    slickUI = game.plugins.add(Phaser.Plugin.SlickUI);
+
+function main() {
+  var game = new Phaser.Game(650, 350, Phaser.CANVAS, 'gamecanvas', {
+    preload: preload,
+    create: create,
+    update: update
+  });
+
+  var SLICKUI = Phaser.Plugin.SlickUI;
+  var sui;
+
+  /**
+   * Preload
+   *
+   *  - load the Plugin
+   *  - load images
+   *  - load JSON for Slick-UI
+   */
+  function preload() {
+
+    sui = game.plugins.add(SLICKUI);
+
     game.load.image('backdrop', 'assets/backdrop.png');
-    slickUI.load('assets/ui/kenney/kenney.json');
-}
-function create() {
-    game.add.sprite(0,-125,'backdrop');
-    var panel;
-    slickUI.add(panel = new SlickUI.Element.Panel(8, 8, game.width - 16, game.height - 16));
-    panel.add(new SlickUI.Element.Text(10,10, "Text input")).centerHorizontally().text.alpha = 0.5;
-    panel.add(new SlickUI.Element.Text(12,34, "Your name"));
-    var textField = panel.add(new SlickUI.Element.TextField(10,58, panel.width - 20, 40));
-    textField.events.onOK.add(function () {
-        alert('Your name is: ' + textField.value);
+
+    sui.load('assets/ui/kenney/kenney.json');
+  }
+
+  var textfield;
+  /**
+   * Create
+   *
+   */
+  function create() {
+    game.add.sprite(0, -125, 'backdrop');
+
+    var panel = new SLICKUI.Panel(
+        game, 8, 8, game.width - 16, game.height - 16);
+    var inputText = new SLICKUI.Text(
+        game, 10, 10, 'Text Input');
+    var nameText = new SLICKUI.Text(
+        game, 12, 34, 'Your Name');
+
+    sui.add(panel);
+    panel.add(inputText);
+    panel.add(nameText);
+
+    inputText.centerHorizontally()
+             .text.alpha = 0.5;
+
+    textfield = new SLICKUI.TextField(
+        game, 10, 58, panel.width - 20, 40, 32, 'QWERTZ');
+
+    panel.add(textfield);
+
+    textfield.events.onOK.add(function() {
+      alert('Your name is: ' + textfield.value);
     });
-    textField.events.onToggle.add(function (open) {
-        console.log('You just ' + (open ? 'opened' : 'closed') + ' the virtual keyboard');
+
+    textfield.events.onToggle.add(function(open) {
+      console.log('You just ' +
+        (open ? 'opened' : 'closed') +
+        ' the virtual keyboard'
+      );
     });
-    textField.events.onKeyPress.add(function(key) {
-        console.log('You pressed: ' + key);
+
+    textfield.events.onKeyPress.add(function(key) {
+      console.log('You pressed: ' + key);
     });
+  }
+
+  /**
+   * Update
+   */
+  function update() {
+    game.debug.spriteInfo(textfield._sprite, 0, 0);
+  }
 }
 
+
+main();
